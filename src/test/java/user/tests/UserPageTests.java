647fb5481fbc2433b2c7a7e7e76e58df72d26288
Antonov Arthur <eichholtz@yandex.ru>
2022-03-29 20:57:30 +0300

    cleaning tests up to 5 pcs

--- src/test/java/user/tests/UserPageTests.java
+++ src/test/java/user/tests/UserPageTests.java
@@ -81,187 +81,187 @@ public class UserPageTests {
         new UserPage(driver)
                 .checkSumButton_3();
     }
-
-    @DisplayName("Клик по ссылке пользовательского соглашения")
-    @Test
-    public void userAgreementLinkTest() {
-        Allure.addAttachment("Скриншот страницы",
-                new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
-        new UserPage(driver)
-                .clickOnUserAgreementLink();
-        new CustomLogger().beforeQuit(driver);
-    }
-
-    @DisplayName("Клик по ссылке Политика обработки персональных данных")
-    @Test
-    public void personalDataAgreementLinkTest() {
-        Allure.addAttachment("Скриншот страницы",
-                new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
-        new UserPage(driver)
-                .clickOnPersonalDataAgreementLink();
-        new CustomLogger().beforeQuit(driver);
-    }
-
-    @DisplayName("Проверка чекбокса издержки")
-    @Test
-    public void userCommissionCheckboxTest() {
-
-        new UserPage(driver)
-                .clickOnUserCommissionCheckbox();
-    }
-
-    @DisplayName("Проверка чекбокса соглашений")
-    @Test
-    public void personalDataAgreementCheckboxTest() {
-        Allure.addAttachment("Скриншот страницы",
-                new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
-        new UserPage(driver)
-                .clickOnPersonalDataAgreementCheckbox();
-        new CustomLogger().beforeQuit(driver);
-    }
-
-    @DisplayName("Удаление суммы")
-    @Test
-    public void deleteSumTest() {
-        new UserPage(driver)
-                .clickDeleteSum()
-                .clickSumButton_1()
-                .clickDeleteSum();
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @DisplayName("Клик по 1-й звезде")
-    @Test
-    public void clickOnFirstStarTest() {
-        new UserPage(driver).clickOnFirstStar();
-    }
-
-    @DisplayName("Клик по 2-й звезде")
-    @Test
-    public void clickOnSecondStarTest() {
-        new UserPage(driver).clickOnSecondStar();
-    }
-
-    @DisplayName("Клик по 3-й звезде")
-    @Test
-    public void clickOnThirdStarTest() {
-        new UserPage(driver).clickOnThirdStar();
-    }
-
-    @DisplayName("Клик по 4-й звезде")
-    @Test
-    public void clickOnFourthStarTest() {
-        new UserPage(driver).clickOnFourthStar();
-    }
-
-    @DisplayName("Клик по 5-й звезде")
-    @Test
-    public void clickOnFifthStarTest() {
-        new UserPage(driver).clickOnFifthStar();
-    }
-
-    @DisplayName("Выбрать пункт Обслуживание")
-    @Test
-    public void clickOnServiceRadioButton() {
-        new UserPage(driver)
-                .clickOnFirstStar()
-                .clickOnServiceRadioButton();
-    }
-
-    @DisplayName("Выбрать пункт Атмосфера")
-    @Test
-    public void clickOnVibeRadioButton() {
-        new UserPage(driver)
-                .clickOnFirstStar()
-                .clickOnVibeRadioButton();
-    }
-
-    @DisplayName("Выбрать пункт Качество блюд")
-    @Test
-    public void clickOnQualityDishesRadioButton() {
-        new UserPage(driver)
-                .clickOnFirstStar()
-                .clickOnQualityDishesRadioButton();
-    }
-
-    @DisplayName("Выбрать пункт Чистота")
-    @Test
-    public void clickOnPurenessRadioButton() {
-        new UserPage(driver)
-                .clickOnFirstStar()
-                .clickOnPurenessRadioButton();
-    }
-
-    @DisplayName("Клик по кнопке рассказать подробнее (5 звезд)")
-    @Test
-    public void checkAddFeedbackBtnOnFiveStars() {
-        new UserPage(driver)
-                .clickOnFifthStar()
-                .checkAddFeedbackBtn();
-    }
-
-    @DisplayName("Клик по кнопке рассказать подробнее (4 звезды)")
-    @Test
-    public void checkAddFeedbackBtn() {
-        new UserPage(driver)
-                .clickOnFourthStar()
-                .checkAddFeedbackBtn();
-    }
-    @DisplayName("Оставить комментарий (1 звездa)")
-    @Test
-    public void checkTextAreaWithOneStars() {
-        new UserPage(driver)
-                .clickOnFirstStar()
-                .checkTextArea();
-        new CustomLogger().beforeQuit(driver);
-    }
-    @DisplayName("Оставить комментарий (2 звезды)")
-    @Test
-    public void checkTextAreaWithTwoStars() {
-        new UserPage(driver)
-                .clickOnSecondStar()
-                .checkTextArea();
-        new CustomLogger().beforeQuit(driver);
-    }
-    @DisplayName("Оставить комментарий (3 звезды)")
-    @Test
-    public void checkTextAreaWithThreeStars() {
-        new UserPage(driver)
-                .clickOnThirdStar()
-                .checkTextArea();
-        new CustomLogger().beforeQuit(driver);
-    }
-    @DisplayName("Оставить комментарий (4 звезды)")
-    @Test
-    public void checkTextAreaWithFourStars() {
-        new UserPage(driver)
-                .clickOnFourthStar()
-                .checkAddFeedbackBtn()
-                .checkTextArea();
-        new CustomLogger().beforeQuit(driver);
-    }
-
-    @DisplayName("Оставить комментарий (5 звезд)")
-    @Test
-    public void checkTextAreaWithFiveStars() {
-        new UserPage(driver)
-                .clickOnFifthStar()
-                .checkAddFeedbackBtn()
-                .checkTextArea();
-        new CustomLogger().beforeQuit(driver);
-    }
-
-    @DisplayName("Клик по кнопке Оплатить картой")
-    @Test
-    public void clickOnPayByCardBtn() {
-        new UserPage(driver)
-                .clickOnPayByCardBtn();
-        new CustomLogger().beforeQuit(driver);
-    }
+//
+//    @DisplayName("Клик по ссылке пользовательского соглашения")
+//    @Test
+//    public void userAgreementLinkTest() {
+//        Allure.addAttachment("Скриншот страницы",
+//                new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+//        new UserPage(driver)
+//                .clickOnUserAgreementLink();
+//        new CustomLogger().beforeQuit(driver);
+//    }
+//
+//    @DisplayName("Клик по ссылке Политика обработки персональных данных")
+//    @Test
+//    public void personalDataAgreementLinkTest() {
+//        Allure.addAttachment("Скриншот страницы",
+//                new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+//        new UserPage(driver)
+//                .clickOnPersonalDataAgreementLink();
+//        new CustomLogger().beforeQuit(driver);
+//    }
+//
+//    @DisplayName("Проверка чекбокса издержки")
+//    @Test
+//    public void userCommissionCheckboxTest() {
+//
+//        new UserPage(driver)
+//                .clickOnUserCommissionCheckbox();
+//    }
+//
+//    @DisplayName("Проверка чекбокса соглашений")
+//    @Test
+//    public void personalDataAgreementCheckboxTest() {
+//        Allure.addAttachment("Скриншот страницы",
+//                new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+//        new UserPage(driver)
+//                .clickOnPersonalDataAgreementCheckbox();
+//        new CustomLogger().beforeQuit(driver);
+//    }
+//
+//    @DisplayName("Удаление суммы")
+//    @Test
+//    public void deleteSumTest() {
+//        new UserPage(driver)
+//                .clickDeleteSum()
+//                .clickSumButton_1()
+//                .clickDeleteSum();
+//        try {
+//            Thread.sleep(500);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @DisplayName("Клик по 1-й звезде")
+//    @Test
+//    public void clickOnFirstStarTest() {
+//        new UserPage(driver).clickOnFirstStar();
+//    }
+//
+//    @DisplayName("Клик по 2-й звезде")
+//    @Test
+//    public void clickOnSecondStarTest() {
+//        new UserPage(driver).clickOnSecondStar();
+//    }
+//
+//    @DisplayName("Клик по 3-й звезде")
+//    @Test
+//    public void clickOnThirdStarTest() {
+//        new UserPage(driver).clickOnThirdStar();
+//    }
+//
+//    @DisplayName("Клик по 4-й звезде")
+//    @Test
+//    public void clickOnFourthStarTest() {
+//        new UserPage(driver).clickOnFourthStar();
+//    }
+//
+//    @DisplayName("Клик по 5-й звезде")
+//    @Test
+//    public void clickOnFifthStarTest() {
+//        new UserPage(driver).clickOnFifthStar();
+//    }
+//
+//    @DisplayName("Выбрать пункт Обслуживание")
+//    @Test
+//    public void clickOnServiceRadioButton() {
+//        new UserPage(driver)
+//                .clickOnFirstStar()
+//                .clickOnServiceRadioButton();
+//    }
+//
+//    @DisplayName("Выбрать пункт Атмосфера")
+//    @Test
+//    public void clickOnVibeRadioButton() {
+//        new UserPage(driver)
+//                .clickOnFirstStar()
+//                .clickOnVibeRadioButton();
+//    }
+//
+//    @DisplayName("Выбрать пункт Качество блюд")
+//    @Test
+//    public void clickOnQualityDishesRadioButton() {
+//        new UserPage(driver)
+//                .clickOnFirstStar()
+//                .clickOnQualityDishesRadioButton();
+//    }
+//
+//    @DisplayName("Выбрать пункт Чистота")
+//    @Test
+//    public void clickOnPurenessRadioButton() {
+//        new UserPage(driver)
+//                .clickOnFirstStar()
+//                .clickOnPurenessRadioButton();
+//    }
+//
+//    @DisplayName("Клик по кнопке рассказать подробнее (5 звезд)")
+//    @Test
+//    public void checkAddFeedbackBtnOnFiveStars() {
+//        new UserPage(driver)
+//                .clickOnFifthStar()
+//                .checkAddFeedbackBtn();
+//    }
+//
+//    @DisplayName("Клик по кнопке рассказать подробнее (4 звезды)")
+//    @Test
+//    public void checkAddFeedbackBtn() {
+//        new UserPage(driver)
+//                .clickOnFourthStar()
+//                .checkAddFeedbackBtn();
+//    }
+//    @DisplayName("Оставить комментарий (1 звездa)")
+//    @Test
+//    public void checkTextAreaWithOneStars() {
+//        new UserPage(driver)
+//                .clickOnFirstStar()
+//                .checkTextArea();
+//        new CustomLogger().beforeQuit(driver);
+//    }
+//    @DisplayName("Оставить комментарий (2 звезды)")
+//    @Test
+//    public void checkTextAreaWithTwoStars() {
+//        new UserPage(driver)
+//                .clickOnSecondStar()
+//                .checkTextArea();
+//        new CustomLogger().beforeQuit(driver);
+//    }
+//    @DisplayName("Оставить комментарий (3 звезды)")
+//    @Test
+//    public void checkTextAreaWithThreeStars() {
+//        new UserPage(driver)
+//                .clickOnThirdStar()
+//                .checkTextArea();
+//        new CustomLogger().beforeQuit(driver);
+//    }
+//    @DisplayName("Оставить комментарий (4 звезды)")
+//    @Test
+//    public void checkTextAreaWithFourStars() {
+//        new UserPage(driver)
+//                .clickOnFourthStar()
+//                .checkAddFeedbackBtn()
+//                .checkTextArea();
+//        new CustomLogger().beforeQuit(driver);
+//    }
+//
+//    @DisplayName("Оставить комментарий (5 звезд)")
+//    @Test
+//    public void checkTextAreaWithFiveStars() {
+//        new UserPage(driver)
+//                .clickOnFifthStar()
+//                .checkAddFeedbackBtn()
+//                .checkTextArea();
+//        new CustomLogger().beforeQuit(driver);
+//    }
+//
+//    @DisplayName("Клик по кнопке Оплатить картой")
+//    @Test
+//    public void clickOnPayByCardBtn() {
+//        new UserPage(driver)
+//                .clickOnPayByCardBtn();
+//        new CustomLogger().beforeQuit(driver);
+//    }
 
 
     @AfterEach
