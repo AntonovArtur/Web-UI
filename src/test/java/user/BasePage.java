d04cb6ffcac2798ea627a2d4c54bdc2f9bede4ab
Antonov Arthur <eichholtz@yandex.ru>
2022-03-29 17:50:47 +0300

    test of cicd  #2

--- src/test/java/user/BasePage.java
+++ src/test/java/user/BasePage.java
@@ -3,6 +3,7 @@ package user;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,12 +16,14 @@ import java.util.Properties;
 
 public class BasePage {
     public WebDriver driver;
+    public Actions actions;
     public WebDriverWait webDriverWait;
     protected static Properties properties = new Properties();
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        actions = new Actions(driver);
         PageFactory.initElements(driver, this);
     }
 
