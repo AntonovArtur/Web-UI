5170d31b7ff26e90333e6e68f8cb0b57d4809ad6
Antonov Arthur <eichholtz@yandex.ru>
2022-03-30 16:49:43 +0300

    changed TOKEN to PERSONAL_TOKEN in a job "Test"

--- src/test/java/user/tests/UserPageTests.java
+++ src/test/java/user/tests/UserPageTests.java
@@ -53,7 +53,7 @@ public class UserPageTests {
                 .checkUserName();
         new CustomLogger().beforeQuit(driver);
     }
-//ывывf
+//ывыв
 //    @DisplayName("Текст 1-й кнопки суммы")
 //    @Test
 //    public void checkSumButton_1() {
