688fd095b51e2c23f529f7037325dc11692729e8
Antonov Arthur <eichholtz@yandex.ru>
2022-03-30 22:29:26 +0300

    All tests pageload up to 15 sec

--- src/test/java/user/tests/UserPageTests.java
+++ src/test/java/user/tests/UserPageTests.java
@@ -20,7 +20,7 @@ import java.util.Properties;
 public class UserPageTests {
     WebDriver driver;
     private static ChromeOptions options = new ChromeOptions();
-    private Duration duration = Duration.ofSeconds(10);
+    private Duration duration = Duration.ofSeconds(15);
 
 
     @BeforeAll
