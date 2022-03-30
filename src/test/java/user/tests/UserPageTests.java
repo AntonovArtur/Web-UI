3887b23160fb490047596088945d995b149d93e4
Antonov Arthur <eichholtz@yandex.ru>
2022-03-30 22:36:40 +0300

    //options.setPageLoadStrategy(PageLoadStrategy.NONE);

--- src/test/java/user/tests/UserPageTests.java
+++ src/test/java/user/tests/UserPageTests.java
@@ -29,6 +29,7 @@ public class UserPageTests {
         System.out.println(new  Properties(System.getProperties()).getProperty("user.dir"));
         System.setProperty("webdriver.chrome.driver",new  Properties(System.getProperties()).getProperty("user.dir") + "/src/main/resources/chromedriver");
         //options.setPageLoadStrategy(PageLoadStrategy.NONE);
+
         options.addArguments("--dns-prefetch-disable",/* "--window-size=600,920", */"--ignore-certificate-errors");
         options.addArguments("--headless","--disable-gpu","--lang=ru","--no-sandbox");
 //
